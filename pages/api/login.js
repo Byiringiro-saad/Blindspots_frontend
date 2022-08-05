fa8e37fc8856d88d6f2d6116ae03982a3a77a082
Byiringiro-saad <byiringirosaad@gmail.com>
2022-08-05 09:17:53 +0200

    code format

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,14 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import dotenv from "dotenv";
 
 import axios from "../../features/axios";
 
-dotenv.config();
-
 export default function handler(req, res) {
   const { username, password } = req.body;
-
-  console.log(username, password);
 
   return new Promise((resolve, reject) => {
     axios
